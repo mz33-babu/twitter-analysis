949df7f5a11ccb3c567818cde772ae1b04b98cd5
Babu <mzee_mkongwe@tutanota.com>
2023-03-20 11:22:51 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import tweepy
+import os
+import time
+import csv
+
+# Twitter API credentials
+consumer_key = "ZWrcObb1CYdRHDduMWwXd00N6"
+consumer_secret = "cGhccSnkmhhmWAtJJKO9bgAQmL1zjyIocGrnhVFlY2gTfIBqQP"
+access_token = "92945738-t0MEa3Q2qmcNHzlHaJ8PGhbeAan5EelfUwCJYQYrp"
+access_secret = "g3d5yIu2WruX62FIcqUHYNDHSPt92X85ZnCCRix0kceAg"
+
+# Authentication to Twitter API
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+# Initialize the API
+api = tweepy.API(auth)
+
+# List of keywords to track
+keywords = ['mbogi', 'genje', 'mbogi bado']
+
+# Define the Stream class
+class Stream(tweepy.Stream):
+    def __init__(self, *args, **kwargs):
+        super().__init__(auth, *args, **kwargs)
+        
+    def on_status(self, status):
+        # Check if the tweet contains one of the keywords
+        if any(keyword in status.text.lower() for keyword in keywords):
+            # Print the tweet details
+            print('Account user:', status.user.name)
+            print('Twitter handle:', status.user.screen_name)
+            print('Tweet:', status.text)
+            print('Time and date:', status.created_at)
+            print('Previous mentions:')
+            # Search for previous tweets from the same user containing the same keyword
+            for tweet in tweepy.Cursor(api.user_timeline, screen_name=status.user.screen_name).items(100):
+                if any(keyword in tweet.text.lower() for keyword in keywords):
+                    print(tweet.text)
+            print('\n\n')
+            # Write the data to a CSV file
+            with open('twitter_data.csv', 'a', newline='', encoding='utf-8') as file:
+                writer = csv.writer(file)
+                writer.writerow([status.user.name, status.user.screen_name, status.text, status.created_at])
+
+# Create the Stream
+stream = tweepy.Stream(auth=auth, tweet_mode='extended')
+
+# Start the Stream
+stream.filter(track=keywords)
